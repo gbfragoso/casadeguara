a4401a76f3f4df76148c37b0b271a8679bbaba4c
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2024-04-20 17:58:29 -0300

    Tela de autores funcionando

--- src/routes/biblioteca/autores/+page.server.ts
+++ src/routes/biblioteca/autores/+page.server.ts
@@ -1,19 +1,46 @@
 import { prisma } from '$lib/server/prisma';
+import { fail, error, redirect } from '@sveltejs/kit';
+
+
 import type { PageServerLoad } from './$types';
 import type { Actions } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+const removeAccents = function (string: String) {
+    return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+export const load: PageServerLoad = async ({ url, params }) => {
+    const nome = removeAccents(url.searchParams.get('nome') || '');
     const autores = await prisma.autor.findMany({
-        take: 10
+        take: 10,
+        where: {
+            nome: {
+                startsWith: nome.toUpperCase()
+            }
+        }
     });
     return { autores };
 }
 
 export const actions: Actions = {
-    editar: async (event) => {
-		console.log("editar");
-	},
-    excluir: async (event) => {
-		console.log("excluir");
-	}
+    excluir: async ({ url }) => {
+        const id = url.searchParams.get("id");
+        if (!id) {
+            return fail(400, { message: 'Nenhum autor selecionado para exclusão' });
+        }
+
+        try {
+            await prisma.autor.delete({
+                where: {
+                    idautor: Number(id)
+                }
+            });
+        } catch (err) {
+            return error(500, {message: 'Falha ao excluir o autor'});
+        }
+
+        return {
+            status: 200
+        }
+    }
 };
--- src/routes/biblioteca/autores/+page.svelte
+++ src/routes/biblioteca/autores/+page.svelte
@@ -1,26 +1,37 @@
 <script lang="ts">
-    import type { PageServerData } from "./$types";
-    export let data: PageServerData;
+	import type { PageServerData } from './$types';
+	export let data: PageServerData;
+
+	$: ({ autores } = data);
 </script>
 
 <div>Consulta autores</div>
 
+<form action="/biblioteca/autores" method="GET">
+	<input type="text" name="nome" id="nome" />
+	<button>Pesquisar</button>
+</form>
+<a href="/biblioteca/autores/novo">
+	<button>Novo</button>
+</a>
 <table>
-    <thead>
-        <th>Nome</th>
-        <th>Ações</th>
-    </thead>
-    <tbody>
-        {#each data.autores as autor}
-            <tr>
-                <td>{autor.nome}</td>
-                <td>
-                    <form method="POST">
-                        <button formaction="?/editar"><i class="fa-solid fa-pen-to-square">Editar</i></button>
-                        <button formaction="?/excluir"><i class="fa-regular fa-trash-can">Excluir</i></button>
-                    </form>
-                </td>
-            </tr>
-        {/each}
-    </tbody>
+	<thead>
+		<th>Nome</th>
+		<th>Ações</th>
+	</thead>
+	<tbody>
+		{#each autores as autor}
+			<tr>
+				<td>{autor.nome}</td>
+				<td>
+					<a href="/biblioteca/autores/{autor.idautor}">
+                        <i class="fa-solid fa-pen-to-square">Editar</i>
+                    </a>
+					<form action="?/excluir&id={autor.idautor}" method="POST">
+						<button><i class="fa-regular fa-trash-can">Excluir</i></button>
+					</form>
+				</td>
+			</tr>
+		{/each}
+	</tbody>
 </table>
