b0cd38fd22dab61cb1051f144edfdc5f278e3ce8
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2024-05-31 10:18:48 -0300

    adicionando vercel

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,31 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
 
-export const prisma = new PrismaClient({
-	log: [
-		{
-			emit: 'event',
-			level: 'query'
-		},
-		{
-			emit: 'stdout',
-			level: 'error'
-		},
-		{
-			emit: 'stdout',
-			level: 'info'
-		},
-		{
-			emit: 'stdout',
-			level: 'warn'
-		}
-	]
-});
+export const prisma = new PrismaClient();
 
-prisma.$on('query', (e) => {
-	console.log('Query: ' + e.query);
-	console.log('Params: ' + e.params);
-	console.log('Duration: ' + e.duration + 'ms');
-});
+//prisma.$on('query', (e) => {
+//	console.log('Query: ' + e.query);
+//	console.log('Params: ' + e.params);
+//	console.log('Duration: ' + e.duration + 'ms');
+//});
 
 export const adapter = new PrismaAdapter(prisma.session, prisma.user);
