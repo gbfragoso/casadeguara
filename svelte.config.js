b29bc1e00e24ddf1b7bc463fa535a3e3dea0e863
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2024-07-21 17:45:21 -0300

    changing runtime

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			runtime: 'edge',
+			runtime: 'nodejs20.x',
 			images: {
 				sizes: [640, 828, 1200, 1920, 3840],
 				formats: ['image/avif', 'image/webp'],
