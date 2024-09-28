47420da2da55ce04c48674af2e97a03916a37b02
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2024-09-28 12:19:34 -0300

    cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,16 @@ declare global {
 			user: import('lucia').User | null;
 			session: import('lucia').Session | null;
 		}
+
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+		}
 	}
 }
 
