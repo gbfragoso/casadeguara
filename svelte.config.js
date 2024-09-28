fd50bea954a65011d154a8d22e6d986df0152801
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2024-09-28 13:51:22 -0300

    processor

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
+	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
-			// See below for an explanation of these options
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
